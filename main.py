b042c8d44b06c2851049269cd927fa24bf5d8e83
Mario Merlano <mario-merlano@hotmail.com>
2024-11-10 11:46:57 -0800

    fix: code refactoring, improve prompt, optimize response times

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import ollama
 import os
 import subprocess
 
+# How this program works?
+# 1. [DONE] Convert natural language to hacking command (nmap or gobuster for now)
+# 2. Checks before executing
+# 3. Execute command
+# 4. Output results in a simple and short way (LLM again)
+
 # Mandatory fields
 domain = input("Domain or IP to attack: ")
 if not domain:
@@ -13,53 +19,65 @@ if not os.path.exists("./SecLists"):
 	print("You must download https://github.com/danielmiessler/SecLists.git")
 	exit()
 
-# Create a variable with all the files from SecLists
-seclists_file_names = ""
-for root, dirs, files in os.walk("./SecLists"):
-	for file in files:
-		# print(os.path.join(root, file))
-		seclists_file_names += os.path.join(root, file) + "\n"
-# print(seclists_file_names)
-
-while True:
+# Get hacking command based on natural language
+def get_hacking_command():
 	# User input from console
 	user_console_input = input("Describe attack: ") # writes output to console
 
 	# Prompt to get only commands based on user input. We also teach how llama must respond on specific scenarios.
 	prompt_to_llama = """
-		System: You are a helpful command line assistant. Provide only the exact command(s) needed, without any explanation or additional text. Do not use markdown formatting. You can also respond only when some argument given by the user is missing.
-        System: For now, you must be limited just to 2 commands: nmap and gobuster with their specific flags and arguments.
-	System: Additional info for gobuster: {}
-	System: The additional info for gobuster are all the possible files that llama can select in order to do a brute-force attack using gobuster. Just select 1 file path and use it in the gobuster command.
-	System: Please note that you must replace the path to wordlist with the actual path to your guessed wordlist file, and ensure that it's in a format compatible with Gobuster.
+		System: You are a command line assistant. You guess commands based on natural language. Provide only the exact command needed, without any explanation or additional text. Do not use markdown formatting.
+		System: You must respond ONLY with one command and its arguments based on common examples: could be nmap or gobuster. Not any other command.
+		
 		User input: {}
-        	User input: {}
-        System: You must only output 1 command.
-	""".format(seclists_file_names, domain, user_console_input)
+        User input: {}
+	""".format(domain, user_console_input)
 
+	print("Loading...")
 	# Send prompt to llama
-	stream = ollama.chat(
-		model='llama3.2',
-		messages=[{'role': 'user', 'content': prompt_to_llama}],
-		stream=True,
-	)
+	response = ollama.chat(model='llama3.2', messages=[
+	{
+		'role': 'user',
+		'content': prompt_to_llama,
+	},
+	])
+	# print(response['message']['content'])
+	hacking_command = response['message']['content']
 	
-	# Return response word by word in the terminal and store it on response_command
-	response_command = ""
-	for chunk in stream:
-		print(chunk['message']['content'], end='', flush=True)
-		response_command += chunk['message']['content']
+	return hacking_command # return hacking command
+
+
+
+# Main loop
+while True:
+	hacking_command = get_hacking_command()
+
+	# check if is gobuster to include dictionaries paths in the prompt
+	if "gobuster" in hacking_command:
+		print("there is a gobuster command")
+		# TODO: add
+		# System: The additional info for gobuster are all the possible files that llama can select in order to do a brute-force attack using gobuster. Just select 1 file path and use it in the gobuster command.
+		# System: Please note that you must replace the path to wordlist with the actual path to your guessed wordlist file, and ensure that it's in a format compatible with Gobuster.
+		# System: Additional info for gobuster: {}
+		# from this variable seclists_file_names
+
+		# Obtain all dictionaries paths
+		seclists_file_names = ""
+		for root, dirs, files in os.walk("./SecLists"):
+			for file in files:
+				# print(os.path.join(root, file))
+				seclists_file_names += os.path.join(root, file) + "\n"
+		# print(seclists_file_names)
 	
-	# Add some separation to the console output
-	print()
-	print()
-	print()
+	# if not, is a nmap
 
-	# Use response_command to execute command
-	print("COMMAND EXECUTION: " + response_command)
-	output = subprocess.check_output([response_command], shell=True)
-
-
-
-exit() ########### REMOVE AFTER USE ###########
-
+	# Use hacking_command to execute command
+	print("COMMAND EXECUTION: " + hacking_command)
+	
+	try:
+		output = subprocess.check_output([hacking_command], shell=True)
+		print(output)
+	except subprocess.CalledProcessError as e:
+		print(f"Command failed with error: {e}")
+		print("Try again!")
+		continue
