9593d76a52e796b692e7b7ef5b1133e777f6e038
Mario Merlano <mario-merlano@hotmail.com>
2024-11-10 15:11:34 -0800

    feat: implement find local guessed directory

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import subprocess
 # 3. Execute command
 # 4. Output results in a simple and short way (LLM again)
 
+
 # Mandatory fields
 domain = input("Domain or IP to attack: ")
 if not domain:
@@ -18,6 +19,7 @@ if not domain:
 if not os.path.exists("./SecLists"):
 	print("You must download https://github.com/danielmiessler/SecLists.git")
 	exit()
+
 
 # call llama
 def llama(prompt):
@@ -31,11 +33,9 @@ def llama(prompt):
 	print(llama_response)
 	return llama_response
 
-# Get hacking command based on natural language
-def get_hacking_command():
-	# User input from console
-	user_console_input = input("Describe attack: ") # writes output to console
 
+# Get hacking command based on natural language
+def get_hacking_command(user_console_input):
 	# Prompt to get only commands based on user input. We also teach how llama must respond on specific scenarios.
 	prompt_to_llama = """
 		System: You are a command line assistant. You guess commands based on natural language. Provide only the exact command needed, without any explanation or additional text. Do not use markdown formatting.
@@ -52,30 +52,43 @@ def get_hacking_command():
 	return hacking_command # return hacking command
 
 
+# Get relative path local dictionary based on a guessed dictionary
+def find_dictionary_local(dictionary_guessed):
+	for root, dirs, files in os.walk("./SecLists"):
+		for file in files:
+			if dictionary_guessed in file:
+				local_dictionary = os.path.relpath(os.path.join(root, file), start="./SecLists")
+				print("Found local dictionary at: ",local_dictionary)
+				return local_dictionary
+
+
 
 # Main loop
 while True:
-	hacking_command = get_hacking_command()
+	# User input from console
+	user_console_input = input("Describe attack: ") # writes output to console
+
+	hacking_command = get_hacking_command(user_console_input)
 
 	# check if is gobuster to include dictionaries paths in the prompt
 	if "gobuster" in hacking_command:
 		print("gobuster command detected!")
 
+		# PROGRAMAR EL DICTIONARY GUESSER. FALLA MUCHO. Solamente hacer un in en todos los directiios y listo
 		print("Guessing dictionary to apply... (it takes a while, please be patient!)")
-		# Interesting: Give me the folders to do a subdomain enumeration from https://github.com/danielmiessler/SecLists
+		dictionary_guessed = llama("You are a dictionary guesser based on the following user request: {}. You must respond with just 1 file name from https://github.com/danielmiessler/SecLists. Provide only the exact file name needed, without any explanation or additional text. Remove everything except the filename and its extension. Just the txt file name with its extension. Do not use markdown formatting.".format(user_console_input))
+		print("Dictionary guessed: ", dictionary_guessed)
+		
+		print("Finding dictionary locally...")
 
 		# System: The additional info for gobuster are all the possible files that llama can select in order to do a brute-force attack using gobuster. Just select 1 file path and use it in the gobuster command.
 		# System: Please note that you must replace the path to wordlist with the actual path to your guessed wordlist file, and ensure that it's in a format compatible with Gobuster.
 		# System: Additional info for gobuster: {}
 		# from this variable seclists_file_names
 
-		# Obtain all dictionaries paths
-		seclists_file_names = ""
-		for root, dirs, files in os.walk("./SecLists"):
-			for file in files:
-				# print(os.path.join(root, file))
-				seclists_file_names += os.path.join(root, file) + "\n"
-		# print(seclists_file_names)
+		local_dictionary = find_dictionary_local(dictionary_guessed)
+
+		exit()
 	
 	# if not, is a nmap
 	print("nmap command detected!")
